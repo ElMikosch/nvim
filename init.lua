1202b01bcb07c6186b9d90473a8981139b370772
Brandon Clark <bwclark97@gmail.com>
2024-07-28 12:43:08 -0400

    Modify conform comments to prevent deprecation warning when used (#1057)

--- init.lua
+++ init.lua
@@ -655,6 +655,7 @@ require('lazy').setup({
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
+        
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
