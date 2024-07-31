eebee090a7d95ce4140fb57eb7ea3c363f65d1db
Mathias Mikosch <mmikosch@dspace.de>
2024-07-31 14:32:47 +0200

    added leetcode and obsidian

--- init.lua
+++ init.lua
@@ -662,7 +662,6 @@ require('lazy').setup({
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
-        
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
